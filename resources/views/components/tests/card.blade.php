a863b050954f25217b3aaa62bf8e7082fd0cb312
TomoyukiWakiyama <kreva2002@gmail.com>
2022-02-28 15:05:20 +0900

    props(初期値)の追加

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,8 @@
+@props([
+    'title' => 'titleの値が設定されておりません。',
+    'content' => 'contentの値が設定されておりません。',
+    'message' => 'messageの値が設定されておりません。',
+    ]);
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,7 +3,9 @@
         ヘッダー１
     </x-slot>
 コンポーネントテスト１
-    <x-tests.card title="カードタイトル"
+    <x-tests.card title="カードタイトル1"
                   content="カードコンテント"
                   :message="$message" />
+    <x-tests.card title="カードタイトル2"
+                  />
 </x-tests.app><!-- どのコンポーネントを使うかファイル名で指定している -->
