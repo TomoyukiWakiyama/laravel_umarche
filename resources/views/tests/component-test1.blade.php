4668ab3bc83b0f1014ba17d42bdd01de5845c3a2
TomoyukiWakiyama <kreva2002@gmail.com>
2022-02-28 14:21:44 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,6 @@
-<x-tests.app>
+<x-tests.app><!-- どのテンプレートを使うか指定している -->
+    <x-slot name="header">
+        ヘッダー１
+    </x-slot>
 コンポーネントテスト１
-</x-tests.app>
+</x-tests.app><!-- どのテンプレートを使うか指定している -->
