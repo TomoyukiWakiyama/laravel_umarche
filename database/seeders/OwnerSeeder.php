ac7cf7c591e8b37316d82d035b1effb922d987c7
TomoyukiWakiyama <kreva2002@gmail.com>
2022-03-04 17:08:43 +0900

    ショップの完全削除時にforeignをcascadeで削除

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -29,42 +29,6 @@ class OwnerSeeder extends Seeder
                 'password' => Hash::make('password'),
                 'created_at' => '2022/02/16 16:16:16'
             ],
-            [
-                'name' => 'test3',
-                'email' => 'test3@fake.com',
-                'password' => Hash::make('password'),
-                'created_at' => '2022/02/16 16:16:16'
-            ],
-            [
-                'name' => 'test4',
-                'email' => 'test4@fake.com',
-                'password' => Hash::make('password'),
-                'created_at' => '2022/02/16 16:16:16'
-            ],
-            [
-                'name' => 'test5',
-                'email' => 'test5@fake.com',
-                'password' => Hash::make('password'),
-                'created_at' => '2022/02/16 16:16:16'
-            ],
-            [
-                'name' => 'test6',
-                'email' => 'test6@fake.com',
-                'password' => Hash::make('password'),
-                'created_at' => '2022/02/16 16:16:16'
-            ],
-            [
-                'name' => 'test7',
-                'email' => 'test7@fake.com',
-                'password' => Hash::make('password'),
-                'created_at' => '2022/02/16 16:16:16'
-            ],
-            [
-                'name' => 'test8',
-                'email' => 'test8@fake.com',
-                'password' => Hash::make('password'),
-                'created_at' => '2022/02/16 16:16:16'
-            ],
         ]);
         
     }
