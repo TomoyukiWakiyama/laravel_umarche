c56831f6601d0d30d8a597e9029330f8b3d7cc9e
TomoyukiWakiyama <kreva2002@gmail.com>
2022-03-12 11:03:56 +0900

    決済成功時のアイテム削除

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,8 @@ Route::prefix('cart')
                 ->name('cart.delete');
         Route::get('checkout', [CartController::class, 'checkout'])
                 ->name('cart.checkout');
+        Route::get('success', [CartController::class, 'success'])
+                ->name('cart.success');
     });
 
 /*********************************
